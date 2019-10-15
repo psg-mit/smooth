f2cf91f87935a0f4d34068b9aba4de52463e15d1
Ben Sherman <sherm@mit.edu>
2019-10-15 10:59:10 -0700

    Add some more documentation

--- src/Diffeo.hs
+++ src/Diffeo.hs
@@ -1,3 +1,8 @@
+{-|
+Forward-mode AD over `CMap`.
+Admits higher-order derivatives, but not higher-order functions.
+-}
+
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE StandaloneDeriving, DeriveFunctor #-}
@@ -24,7 +29,7 @@ import qualified MPFR as M
 
 infixr :#
 
-{-- `Df a a b ()` is a Taylor series for a smooth function `f : a -> b`.
+{-| `Df a a b ()` is a Taylor series for a smooth function `f : a -> b`.
   It is represented by a stream of continuous maps, for the function
   and all its derivatives:
 
@@ -55,10 +60,17 @@ infixr :#
   derivative.
 -}
 data Df g a b k = CMap (g, k) b :# Df g a b (a, k)
+
+{-| `a :~> b` represents a smooth map from `a` to `b`,
+    as a tower of continuous maps for its derivatives.
+-}
 newtype a :~> b = D (Df a a b ())
 
+{-| A typeclass for the structure of vector spaces
+    needed for computing derivatives.
+-}
 class Additive v where
-  zeroV   :: CMap g v         -- the zero vector
+  zeroV  :: CMap g v         -- the zero vector
   addV   :: CMap (v, v) v    -- add vectors
 
 instance Additive () where
@@ -72,21 +84,29 @@ instance (Additive u, Additive v) => Additive (u, v) where
     v <- addV -< (v1, v2)
     returnA -< (u, v)
 
-class Additive v => VectorSpace v s | v -> s where
-  (*^)    :: CMap (s, v) v    -- scale a vector
-
 instance R.Rounded a => Additive (Interval a) where
   zeroV = 0
   addV = RE.add
 
-instance R.Rounded a => VectorSpace (Interval a) (Interval a) where
-  (*^) = RE.mul
-
+{-| Derivative towers form a vector space. Here we show
+    that there is a zero `dZero`, and that you can add
+    two derivative towers with `dSum`.
+ -}
 dZero :: Additive b => Df a a' b k
 dZero = zeroV :# dZero
 
 dSum :: Additive b => Df a a' b k -> Df a a' b k -> Df a a' b k
 dSum (f :# f') (g :# g') = RE.ap2 addV f g :# dSum f' g'
+
+{-| A notion of vector spaces over some scalar type `s`,
+    which we use to implement scalar multiplication as
+    a smooth function.
+-}
+class Additive v => VectorSpace v s | v -> s where
+  (*^)    :: CMap (s, v) v    -- scale a vector
+
+instance R.Rounded a => VectorSpace (Interval a) (Interval a) where
+  (*^) = RE.mul
 
 scalarMult :: VectorSpace v s => Df g g' s k -> Df g g' v k -> Df g g' v k
 scalarMult s@(s0 :# s') x@(x0 :# x') =
@@ -129,9 +149,14 @@ dlinearWkn' z (f :# f') = z f :# dlinearWkn' z' f'
     -- g1 :: CMap ((d, (a, x)), k) b
     g1 = z g'
 
+{-| The identity function as a smooth map -}
 dId :: Additive u => u :~> u
 dId = D $ arr fst :# arr (fst . snd) :# dZero
 
+{-| Given a continuous map `f : u -> v` that is linear,
+    its smooth map representation is a derivative tower
+    where f' = f and f'' = f''' = ... = 0.
+-}
 linearD :: Additive v => CMap u v -> u :~> v
 linearD f = D $ (f <<< arr fst) :# (f <<< arr (fst . snd)) :# dZero
 
@@ -150,11 +175,20 @@ pairD (D f) (D g) = D (pairD' f g)
 add :: R.Rounded a => (Interval a, Interval a) :~> Interval a
 add = linearD RE.add
 
-
+{-| Composition of two smooth maps yields a smooth map -}
 (@.) :: Additive c => (b :~> c) -> (a :~> b) -> (a :~> c)
 (D g@(g0 :# g')) @. (D f@(f0 :# f')) = D $
   (g0 <<< (f0 &&& arr snd)) :# linCompose (wknValue (f0 <<< (C.id &&& arr (\_ -> ()))) g') f'
 
+{-| Composition of linear maps and their derivative towers yields
+    a linear map with a derivative tower. Essentially, given two linear maps
+    `g : b -> c` and `f : a -> b` with derivative towers, their composition
+    is the derivative tower
+    (g . f) = g . f
+    (g . f)' = (g' . f) + (g + f')
+
+    But there is some important "bookkeeping" and details beyond this.
+-}
 linCompose :: Additive c => Df g b c (b, ka) -> Df g a b (a, ka) -> Df g a c (a, ka)
 linCompose g@(g0 :# g') f@(f0 :# f') =
   (g0 <<< (arr fst &&& f2)) :# dSum (linCompose (dWkn1 f0' g') (dWkn (arr snd) f))
@@ -167,9 +201,12 @@ linCompose g@(g0 :# g') f@(f0 :# f') =
      b'ka' <- f2 -< (g, (a, ka))
      returnA -< (b1, b'ka')
 
+{-| Apply a smooth function of 1 variable to a smooth argument. -}
 dap1 :: Additive b => a :~> b -> g :~> a -> g :~> b
 dap1 f = (f @.)
 
+{-| Apply a smooth function of 2 variables to two smooth
+    arguments at the same stage of definition. -}
 dap2 :: Additive c => (a, b) :~> c -> g :~> a -> g :~> b -> g :~> c
 dap2 f x y = f @. pairD x y
 
@@ -205,5 +242,8 @@ deriv' (f :# f') = dWkn ((zeroV &&& 1) &&& C.id) f'
 deriv :: Additive g => R.Rounded a => (g, Interval a) :~> b -> (g, Interval a) :~> b
 deriv (D f) = D (deriv' f)
 
+{-| An example function. Calculates the `n`th derivative of
+    (\x -> exp (2 * x)) at x = 0.
+-}
 diffeoExample :: Int -> IO ()
 diffeoExample n = E.runAndPrint $ E.asMPFR $ getDerivTower (exp' @. linearD ((*2) C.id)) (E.asMPFR 0) !! n
