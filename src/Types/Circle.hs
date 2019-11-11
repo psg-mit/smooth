e9424a782af9fb6edfa3379b7fa6834fc7356198
Ben Sherman <sherm@mit.edu>
2019-11-11 11:42:35 -0500

    More efficient implementation of the circle

--- src/Types/Circle.hs
+++ src/Types/Circle.hs
@@ -3,41 +3,34 @@
 module Types.Circle where
 
 import Prelude hiding (Real)
-import FwdMode ((:~>), pairD, fstD, sndD)
+import FwdMode ((:~>), pairD, fstD, sndD, dId)
 import FwdPSh
 import MPFR (Real)
 
 
--- Circle = { (x, y) : R^2 | x^2 + y^2 = 1 }
-data Circle g = Circle (DReal g) (DReal g)
+-- Circle = Real quotiented by the equivalence relation
+-- x ~ y := x - y = 2 * k * pi  for some integer k
+newtype Circle g = Circle (DReal g)
 
 fromAngle :: DReal g -> Circle g
-fromAngle theta = Circle (cos theta) (sin theta)
+fromAngle theta = Circle theta
 
 rotate :: DReal g -> Circle g -> Circle g
-rotate theta (Circle x y) = Circle (cos theta * x + sin theta * y)
-                                   (cos theta * y - sin theta * x)
+rotate deltaTheta (Circle theta) = Circle (theta + deltaTheta)
 
 toXY :: Circle g -> (DReal :* DReal) g
-toXY (Circle x y) = x :* y
+toXY (Circle theta) = cos theta :* sin theta
 
 -- Point on circle plus change in angle to tangent bundle
 toTan :: Circle g -> DReal g -> Tan Circle g
-toTan (Circle (R x) (R y)) (R dtheta) = Tan (pairD (pairD x y) (pairD dx dy))
-  (Circle (R fstD) (R sndD))
-  where
-  dx = -y * dtheta
-  dy =  x * dtheta
+toTan (Circle (R theta)) (R dtheta) = Tan (pairD theta dtheta) (Circle (R dId))
 
 -- From tangent bundle to point on circle plus angle
 fromTan :: Tan Circle g -> (Circle :* DReal) g
-fromTan (Tan udu (Circle x' y')) = Circle x y :* theta  where
-  xdx :* ydy = tanProdfrom (Tan udu (x' :* y'))
-  x :* dx = tanRfrom xdx
-  y :* dy = tanRfrom ydy
-  theta = x * dy - y * dx
+fromTan (Tan udu (Circle f)) = Circle theta :* dtheta where
+  theta :* dtheta = tanRfrom (Tan udu f)
 
 -- The exponential map for the circle
 exponentialMap :: Tan Circle g -> Circle g
-exponentialMap t = rotate theta (Circle x y) where
-  Circle x y :* theta = fromTan t
+exponentialMap t = rotate dtheta (Circle theta) where
+  Circle theta :* dtheta = fromTan t
